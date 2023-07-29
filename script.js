1969f4f5e4c71d200857d9f5e2ffbd31ec11400c
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2023-07-29 13:49:17 +0400

    finale:))

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 const mySquare=document.getElementById('Square'); 
 var appendTens = document.getElementById("tens");
 var appendSeconds = document.getElementById("seconds");
-// var lefttopsquare=document.querySelector(".left-top-square");
-// var righttopsquare=document.querySelector(".right-top-square");
-// var leftbottonsquare=document.querySelector(".left-bottom-square");
-// var rightbottomsquare=document.querySelector(".right-bottom-square");
 const dom=document.querySelectorAll(".dom");
-
-
 var div = document.querySelector('.Square'), 
         x = 0, 
         y = 0, 
@@ -17,7 +11,7 @@ var tens=0;
 var sec=0;
 var gamestart=true;
 var rec;
-var rectspeed=10;
+var rectspeed=0;
 
 function startTimer () {
     if(gamestart){
@@ -27,22 +21,19 @@ function startTimer () {
         if(tens>100){
         appendSeconds.innerHTML=Math.round(tens/100);
         appendTens.innerHTML = tens%100;
+        console.log(tens);
         }
-        if(tens>500 && tens<0){
+        if(tens>200 && tens<500){
             rectspeed=2;
-            
         }
-        if(tens>500&& tens<1000){
+        if(tens>500&& tens<1500){
             rectspeed=3;
-            
         }
-        if(tens>1000 && tens<1500){
+        if(tens>1500 && tens<2500){
             rectspeed=4;
-            
         }
-        if(tens>1500 && tens<2000){
+        if(tens>2500 && tens<3000){
             rectspeed=5;
-            
         }
 }
 }
@@ -51,52 +42,7 @@ function Collisiontobox(xs,ys){
     if( xs>270 || xs<0 || ys>370|| ys<0){
         gameover();
         }
-};
-
-function gameover(){
-    gamestart=false;
-    alert(`You survived ${appendSeconds.innerHTML+":"+appendTens.innerHTML} Seconds`);
-    window.location.reload();
-};
- // div event mousedown 
-div.addEventListener('mousedown', function (e) { 
-    gamestart=true;
-    var Interval ; 
-    clearInterval(rec);
-    rec=setInterval(moverect,10) ;
-    clearInterval(Interval);
-    Interval = setInterval(startTimer, 10);
-    // mouse state set to true 
-    mousedown = true; 
-    // subtract offset 
-    x = div.offsetLeft - e.clientX; 
-    y = div.offsetTop - e.clientY; 
-    
-}, true); 
-
-// var rec;  
-// rec=setInterval(moverect,1000) ;
-// div event mouseup 
-// div.addEventListener('mouseup', function (e) { 
-//     // mouse state set to false 
-//     mousedown = false; 
-// }, true); 
-
-mySquare.addEventListener('mousemove', function (e) { 
-    e.preventDefault();
-    
-    gamestart=true;
-    // Is mouse pressed 
-    if (mousedown && gamestart) { 
-        // Now we calculate the difference upwards 
-        div.style.left = e.clientX + x  ; 
-        div.style.top = e.clientY + y ; 
-        Collisiontobox(div.offsetLeft,div.offsetTop);
-        
-    }
-    
-}, true);  
-
+}
 function Collisionsquare(){
     dom.forEach(rect =>{
         if (div.offsetLeft <= rect.offsetLeft + rect.offsetWidth  && 
@@ -113,9 +59,7 @@ function chengmovedir(){
         if(rect.offsetLeft +rect.offsetWidth> 300){
             if(rect.getAttribute('dx')>0)
                 rect.setAttribute('dx',-1*rect.getAttribute('dx'));
-    
         }
-        
         if(rect.offsetLeft<1){
             if(rect.getAttribute('dx')>0)
                 rect.setAttribute('dx',-1*rect.getAttribute('dx'));
@@ -128,14 +72,10 @@ function chengmovedir(){
                 rect.setAttribute('dy',-1*rect.getAttribute('dy'));
         }
         if(rect.offsetTop<1){
-            // if(rect.getAttribute("dy") == 1)
-            //     rect.setAttribute("dy","-1");
-            // if(rect.getAttribute("dy") == -1)
-            //     rect.setAttribute("dy","1");
             if(rect.getAttribute('dy')>0)
-            rect.setAttribute('dy',-1*rect.getAttribute('dy'));
+                rect.setAttribute('dy',-1*rect.getAttribute('dy'));
 
-        if(rect.getAttribute('dy')<0)
+            if(rect.getAttribute('dy')<0)
             rect.setAttribute('dy',-1*rect.getAttribute('dy'));
     }
     
@@ -149,10 +89,7 @@ function moverect(){
             rect.style.top =rect.offsetTop + parseInt(rect.getAttribute("dy")) + "px";
             chengmovedir();
             Collisionsquare();
-            // if(Collisionsquare()){
-            //     mousedown=false;
-            //     gamestart=false;
-            //     gameover();}
+            
             if(rectspeed==2){
                 if(rect.getAttribute('dx')>0)
                     rect.setAttribute('dx','1.5');
@@ -193,15 +130,46 @@ function moverect(){
                 if(rect.getAttribute('dy')<0)
                     rect.setAttribute('dy','-3');
             }
-            
+            console.log(rect.getAttribute('dx'));
         });
         
-            // if(rectspeed==5){
-            //     //console.log(tens,rectspeed);
-            //     //clearInterval(rec);
-            //     //rec=clearInterval(moverect,0) ;}
-            //     //moverect.setSpeed(127);
-                
-            // }
     }}
+
+function gameover(){
+    gamestart=false;
+    alert(`You survived ${appendSeconds.innerHTML+":"+appendTens.innerHTML} Seconds`);
+    window.location.reload();
+}
+ // div event mousedown 
+div.addEventListener('mousedown', function (e) { 
+    e.preventDefault();
+    gamestart=true;
+    var Interval ; 
+    clearInterval(rec);
+    rec=setInterval(moverect,10) ;
+    clearInterval(Interval);
+    Interval = setInterval(startTimer, 10);
+    // mouse state set to true 
+    mousedown = true; 
+    // subtract offset 
+    x = div.offsetLeft - e.clientX; 
+    y = div.offsetTop - e.clientY; 
+    
+}, true); 
+
+mySquare.addEventListener('mousemove', function (e) { 
+    e.preventDefault();
+    
+    gamestart=true;
+    // Is mouse pressed 
+    if (mousedown && gamestart) { 
+        // Now we calculate the difference upwards 
+        div.style.left = e.clientX + x  ; 
+        div.style.top = e.clientY + y ; 
+        Collisiontobox(div.offsetLeft,div.offsetTop);
+        
+    }
+    
+}, true);  
+
 
